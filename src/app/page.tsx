f9c81e10f17cbbd220ce3ed167c75607dcaa9cac
Igzanotto <ignaciozanotto@gmail.com>
2024-04-15 21:26:40 -0300

    front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,23 @@ export default function Chat() {
           <Image
             src="/dog_doctor_logos.png"
             alt="Dog Doctor"
-            width={500} 
+            width={500}
             height={500}
           />
         </div>
-        <div className="" style={{ width: '500px' }}>
-          <h1 className="text-2xl font-bold text-black">Dogtor</h1>
-          <p className="my-2">
-            Cuentame que le pasa a tu peludito asi puedo ayudarte!
-          </p>
+        <div className="" style={{ width: "500px" }}>
+          <h1 className="text-2xl font-bold text-black mt-5">Dogtor</h1>
+
           <form onSubmit={handleSubmit} className="">
             <div className="max-h-96 h-full overflow-y-auto ">
+            <div
+            className={`flex flex-col mb-2 p-2 rounded-md self-end bg-slate-100`}
+          >
+            <span className={"text-xs text-right font-bold"}>dogtor</span>{" "}
+            <p className="my-2">
+              Cuéntame que le pasa a tu peludito asi puedo ayudarte!
+            </p>
+          </div>
               {messages.map((m) => (
                 <div
                   key={m.id}
@@ -38,14 +44,12 @@ export default function Chat() {
                 >
                   <span
                     className={`text-xs ${
-                      m.role === "assistant" 
-                      ? "text-right font-bold" 
-                      : "text-left font-bold"
+                      m.role === "assistant"
+                        ? "text-right font-bold"
+                        : "text-left font-bold"
                     }`}
                   >
-                    {m.role === "assistant"
-                      ? "dogtor"
-                      : "tú"}
+                    {m.role === "assistant" ? "dogtor" : "tú"}
                   </span>{" "}
                   {m.content}
                 </div>
